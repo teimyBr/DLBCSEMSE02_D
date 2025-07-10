c18abbd9a0fb6f344f25aa593d5e204df3220f95
Lucas Herber <lucas.herber@t-online.de>
2025-07-10 20:37:38 +0200

    Add initial version of api

---
+++ Backend/app/api.py
@@ -0,0 +1,51 @@
+def authenticate(name, password):
+    # TODO implement, most likely going to return the player
+    return
+
+def register(payload):
+    #TODO implement, add new player to db, payload is a json or something representing a player
+    return
+
+def getAppointments():
+    # TODO implement, vielleicht alte appointments auf die letzen fünf verringern
+    return
+
+def addAppointment(payload):
+    #TODO implement, payload is a json of a appointment
+    return
+
+def getGameSuggestions(appointmentId):
+    # TODO implement, return list of game suggestions for the appointment
+    return
+
+def addGameSuggestions(payload):
+    # TODO implement, payload list of game suggestions
+    return
+
+def getGames():
+    # TODO implement, return list of games in db
+    return
+
+def addGameVote(payload):
+    # TODO implement, payload is a json that represents a game_vote
+    return
+
+def getGameVotes(player_id, appointment_id):
+    # TODO implement
+    return
+
+def addFoodChoice(player_id, appointment_id, foodDirection_id):
+    # TODO implement
+    return
+
+def getFoodChoices(appointment_id):
+    # TODO implement
+    return
+
+def addEvaluation(payload):
+    # TODO implement
+    return
+
+def addMessage(payload):
+    # TODO implement
+    return
