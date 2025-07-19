3f45549faef42a76e2bcb2039be623a853169817
Lucas Herber <lucas.herber@t-online.de>
2025-07-19 12:37:51 +0200

    Improve handling of non positive response from backend

--- Frontend/BoardGamer/app/src/main/java/com/boardgamer/api/BackendAPI.kt
+++ Frontend/BoardGamer/app/src/main/java/com/boardgamer/api/BackendAPI.kt
@@ -1,5 +1,6 @@
 package com.boardgamer.api
 
+import android.util.Log
 import com.boardgamer.model.Appointment
 import com.boardgamer.model.Evaluation
 import com.boardgamer.model.FoodChoice
@@ -19,125 +20,254 @@ import io.ktor.client.request.setBody
 import io.ktor.client.statement.HttpResponse
 import io.ktor.client.statement.bodyAsText
 import io.ktor.http.ContentType
+import io.ktor.http.HttpStatusCode
 import io.ktor.http.contentType
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.boolean
 import kotlinx.serialization.json.jsonPrimitive
 import kotlinx.serialization.json.long
+import java.net.ConnectException
 
 //For more info about the basicAddress see: https://developer.android.com/studio/run/emulator-networking
 class BackendAPI(
     val basicAddress: String = "http://10.0.2.2:8000"
 ) {
+    val TAG = "BackendAPI"
     val client = HttpClient(CIO)
 
-    suspend fun authenticate(name: String, password: String): Player {
-        return Json.decodeFromString(
-            client.post("$basicAddress/authenticate/${name.replace(" ", "%20")}/$password")
-                .bodyAsText()
-        )
+    suspend fun authenticate(name: String, password: String): Player? {
+        try {
+            val response =
+                client.post("$basicAddress/authenticate/${name.replace(" ", "%20")}/$password")
+            if (response.status == HttpStatusCode.OK) {
+                return Json.decodeFromString(response.bodyAsText())
+            } else {
+                Log.i(
+                    TAG,
+                    "Something went wrong when trying to authenticate, ${response.status}, ${response.bodyAsText()}"
+                )
+                return null
+            }
+        } catch (e: ConnectException) {
+            Log.i(TAG, "Could not connect to backend", e)
+            return null
+        }
     }
 
     suspend fun register(player: RegistrationPlayer): Long {
-        val response = post("$basicAddress/register/", player.toJson())
-        return getIdFromResponse(response)
+        try {
+            val response = post("$basicAddress/register/", player.toJson())
+            if (response == HttpStatusCode.OK) {
+                return getIdFromResponse(response)
+            } else {
+                Log.i(
+                    TAG,
+                    "Failed to register player, ${response.status}, ${response.bodyAsText()}"
+                )
+                return -1
+            }
+        } catch (e: ConnectException) {
+            Log.i(TAG, "Could not connect to backend", e)
+            return -1
+        }
     }
 
     suspend fun getAppointments(): List<Appointment> {
-        return Json.decodeFromString(client.get("$basicAddress/appointments").bodyAsText())
+        val response = client.get("$basicAddress/appointments")
+        if (response.status == HttpStatusCode.OK) {
+            return Json.decodeFromString(response.bodyAsText())
+        } else {
+            Log.i(
+                TAG,
+                "Could not retrieve appointments, ${response.status}, ${response.bodyAsText()}"
+            )
+            return listOf()
+        }
     }
 
     suspend fun addAppointment(appointment: Appointment): Long {
         val response = post("$basicAddress/appointments/insert/", appointment.toJson())
-        return getIdFromResponse(response)
+        if (response.status == HttpStatusCode.OK) {
+            return getIdFromResponse(response)
+        } else {
+            Log.i(TAG, "Failed to add appointment, ${response.status}, ${response.bodyAsText()}")
+            return -1
+        }
     }
 
     suspend fun updateAppointment(appointment: Appointment): Boolean {
         val response = post("$basicAddress/appointments/update/", appointment.toJson())
-        return getBooleanFromResponse(response)
+        if (response.status == HttpStatusCode.OK) {
+            return getBooleanFromResponse(response)
+        } else {
+            Log.i(TAG, "Failed to update appointment, ${response.status}, ${response.bodyAsText()}")
+            return false
+        }
     }
 
     suspend fun getGameSuggestions(appointmentId: Long): List<GameSuggestion> {
-        return Json.decodeFromString(
-            client.get("$basicAddress/gameSuggestions/$appointmentId").bodyAsText()
-        )
+        val response = client.get("$basicAddress/gameSuggestions/$appointmentId")
+        if (response.status == HttpStatusCode.OK) {
+            return Json.decodeFromString(response.bodyAsText())
+        } else {
+            Log.i(
+                TAG,
+                "Failed to get game suggestions, ${response.status}, ${response.bodyAsText()}"
+            )
+            return listOf()
+        }
     }
 
     suspend fun addGameSuggestions(gameSuggestions: List<GameSuggestion>): Boolean {
         val response =
             post("$basicAddress/gameSuggestions/insert/", Json.encodeToString(gameSuggestions))
-        return getBooleanFromResponse(response)
+        if (response.status == HttpStatusCode.OK) {
+            return getBooleanFromResponse(response)
+        } else {
+            Log.i(
+                TAG,
+                "Failed to add game suggestion, ${response.status}, ${response.bodyAsText()}"
+            )
+            return false
+        }
     }
 
     suspend fun getGames(): List<Game> {
-        return Json.decodeFromString(client.get("$basicAddress/games").bodyAsText())
+        val response = client.get("$basicAddress/games")
+        if (response.status == HttpStatusCode.OK) {
+            return Json.decodeFromString(response.bodyAsText())
+        } else {
+            Log.i(TAG, "Failed to get Games, ${response.status}, ${response.bodyAsText()}")
+            return listOf()
+        }
     }
 
     suspend fun addGameVote(gameVote: GameVote): Long {
         val response = post("$basicAddress/gameVotes/insert/", gameVote.toJson())
-        return getIdFromResponse(response)
+        if (response.status == HttpStatusCode.OK) {
+            return getIdFromResponse(response)
+        } else {
+            Log.i(TAG, "Add Game Vote failed, ${response.status}, ${response.bodyAsText()}")
+            return -1
+        }
     }
 
     suspend fun updateGameVote(gameVote: GameVote): Boolean {
         val response = post("$basicAddress/gameVotes/update/", gameVote.toJson())
-        return getBooleanFromResponse(response)
+        if (response.status == HttpStatusCode.OK) {
+            return getBooleanFromResponse(response)
+        } else {
+            Log.i(TAG, "Updating Game Vote failed, ${response.status}, ${response.bodyAsText()}")
+            return false
+        }
     }
 
     suspend fun getGameVotesForPlayer(appointmentId: Long, playerId: Long): List<GameVote> {
-        return Json.decodeFromString(
-            client.get("$basicAddress/gameVotes/$appointmentId/$playerId").bodyAsText()
-        )
+        val response = client.get("$basicAddress/gameVotes/$appointmentId/$playerId")
+        if (response.status == HttpStatusCode.OK) {
+            return Json.decodeFromString(response.bodyAsText())
+        } else {
+            Log.i(
+                TAG,
+                "couldn't get game votes for player, ${response.status}, ${response.bodyAsText()}"
+            )
+            return listOf()
+        }
     }
 
     suspend fun getGameVotesForAppointment(appointmentId: Long): List<GameVote> {
-        return Json.decodeFromString(
-            client.get("$basicAddress/gameVotes/$appointmentId").bodyAsText()
-        )
+        val response = client.get("$basicAddress/gameVotes/$appointmentId")
+        if (response.status == HttpStatusCode.OK) {
+            return Json.decodeFromString(response.bodyAsText())
+        } else {
+            Log.i(
+                TAG,
+                "Couldn't get GameVotes for Appointment, ${response.status}, ${response.bodyAsText()}"
+            )
+            return listOf()
+        }
     }
 
     suspend fun getFoodDirections(): List<FoodDirection> {
-        return Json.decodeFromString(client.get("$basicAddress/foodDirections").bodyAsText())
+        val response = client.get("$basicAddress/foodDirections")
+        if (response.status == HttpStatusCode.OK) {
+            return Json.decodeFromString(response.bodyAsText())
+        } else {
+            Log.i(TAG, "Couldn't get food directions, ${response.status}, ${response.bodyAsText()}")
+            return listOf()
+        }
     }
 
     suspend fun addFoodChoice(appointmentId: Long, playerId: Long, foodDirectionId: Long): Long {
         val response =
             client.post("$basicAddress/foodChoices/insert/$appointmentId/$playerId/$foodDirectionId")
-        return getIdFromResponse(response)
+        if (response.status == HttpStatusCode.OK) {
+            return getIdFromResponse(response)
+        } else {
+            Log.i(TAG, "addFoodChoice failed, ${response.status}, ${response.bodyAsText()}")
+            return -1
+        }
     }
 
     suspend fun getFoodChoices(appointmentId: Long): List<FoodChoice> {
-        return Json.decodeFromString(
-            client.get("$basicAddress/foodChoices/$appointmentId").bodyAsText()
-        )
+        val response = client.get("$basicAddress/foodChoices/$appointmentId")
+        if (response.status == HttpStatusCode.OK) {
+            return Json.decodeFromString(response.bodyAsText())
+        } else {
+            Log.i(TAG, "Failed to getFoodChoices, ${response.status}, ${response.bodyAsText()}")
+            return listOf()
+        }
     }
 
-    suspend fun getFoodChoice(appointmentId: Long, playerId: Long): FoodChoice {
-        return Json.decodeFromString(
-            client.get("$basicAddress/foodChoices/$appointmentId/$playerId").bodyAsText()
-        )
+    suspend fun getFoodChoice(appointmentId: Long, playerId: Long): FoodChoice? {
+        val response = client.get("$basicAddress/foodChoices/$appointmentId/$playerId")
+        if (response.status == HttpStatusCode.OK) {
+            return Json.decodeFromString(response.bodyAsText())
+        } else {
+            Log.i(TAG, "Failed to get Food Choices, ${response.status}, ${response.bodyAsText()}")
+            return null
+        }
     }
 
     suspend fun addEvaluation(evaluation: Evaluation): Long {
         val response = post("$basicAddress/evaluations/insert/", evaluation.toJson())
-        return getIdFromResponse(response)
+        if (response.status == HttpStatusCode.OK) {
+            return getIdFromResponse(response)
+        } else {
+            Log.i(TAG, "Failed to add evaluation, ${response.status}, ${response.bodyAsText()}")
+            return -1
+        }
     }
 
     suspend fun getEvaluations(appointmentId: Long): List<Evaluation> {
-        return Json.decodeFromString(
-            client.get("$basicAddress/evaluations/$appointmentId").bodyAsText()
-        )
+        val response = client.get("$basicAddress/evaluations/$appointmentId")
+        if (response.status == HttpStatusCode.OK) {
+            return Json.decodeFromString(response.bodyAsText())
+        } else {
+            Log.i(TAG, "Failed to get Evaluations, ${response.status}, ${response.bodyAsText()}")
+            return listOf()
+        }
     }
 
     suspend fun addMessage(message: Message): Long {
         val response = post("$basicAddress/messages/insert/", message.toJson())
-        return getIdFromResponse(response)
+        if (response.status == HttpStatusCode.OK) {
+            return getIdFromResponse(response)
+        } else {
+            Log.i(TAG, "Failed to add Message, ${response.status}, ${response.bodyAsText()}")
+            return -1
+        }
     }
 
     suspend fun getMessages(appointmentId: Long): List<Message> {
-        return Json.decodeFromString(
-            client.get("$basicAddress/messages/$appointmentId").bodyAsText()
-        )
+        val response = client.get("$basicAddress/messages/$appointmentId")
+        if (response.status == HttpStatusCode.OK) {
+            return Json.decodeFromString(response.bodyAsText())
+        } else {
+            Log.i(TAG, "Failed to get Messages, ${response.status}, ${response.bodyAsText()}")
+            return listOf()
+        }
     }
 
     private suspend fun getIdFromResponse(response: HttpResponse): Long {
