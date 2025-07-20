4f163bdaf8891b67261f1568df856304d228dfbf
Erik <betacom16@outlook.de>
2025-07-20 13:06:07 +0200

    Inhalte ins ViewModel ausgelagert und erweitert. Strings in die strings.xml ausgelagert

--- Frontend/BoardGamer/app/src/main/java/com/boardgamer/ui/Home.kt
+++ Frontend/BoardGamer/app/src/main/java/com/boardgamer/ui/Home.kt
@@ -6,21 +6,26 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.boardgamer.viewmodel.LoginState
-import com.boardgamer.viewmodel.LoginViewModel
+import com.boardgamer.viewmodel.HomeViewModel
+import com.boardgamer.R
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 @Composable
 fun Home(navController: NavController) {
-    val viewModel: LoginViewModel = viewModel()
-    val loginState by viewModel.loginState.collectAsState()
+    val viewModel: HomeViewModel = viewModel()
 
-    var username by remember { mutableStateOf("") }
-    var password by remember { mutableStateOf("") }
+    val loginState by viewModel.loginState.collectAsState()
+    val username by viewModel.username.collectAsState()
+    val password by viewModel.password.collectAsState()
+    val isButtonEnabled by viewModel.isLoginButtonEnabled.collectAsState()
 
     LaunchedEffect(loginState) {
         if (loginState is LoginState.Success) {
@@ -46,14 +51,14 @@ fun Home(navController: NavController) {
         ) {
 
             Text(
-                text = "Willkommen bei BoardGamer",
+                text = stringResource(id = R.string.app_welcome),
                 fontSize = 25.sp
             )
 
             Spacer(modifier = Modifier.height(48.dp))
 
             Text(
-                text = "Melde dich direkt an",
+                text = stringResource(id = R.string.login_now),
                 fontSize = 18.sp
             )
 
@@ -61,32 +66,43 @@ fun Home(navController: NavController) {
 
             OutlinedTextField(
                 value = username,
-                onValueChange = { username = it },
-                label = { Text("Name") },
+                onValueChange = { viewModel.onUsernameChange(it) },
+                label = {
+                    Text(
+                        text = stringResource(R.string.name)
+                    )
+                },
             )
 
             Spacer(modifier = Modifier.height(8.dp))
 
             OutlinedTextField(
                 value = password,
-                onValueChange = { password = it },
-                label = { Text("Passwort") },
+                onValueChange = { viewModel.onPasswordChange(it) },
+                label = {
+                    Text(
+                        text = stringResource(R.string.password)
+                    )
+                },
                 visualTransformation = PasswordVisualTransformation(),
             )
 
             Spacer(modifier = Modifier.height(16.dp))
 
             Button(
-                onClick = { viewModel.loginUser(username, password) },
+                onClick = { viewModel.loginPlayer() },
                 shape = RectangleShape,
-                enabled = loginState !is LoginState.Loading
+                enabled = isButtonEnabled
             ) {
                 if (loginState is LoginState.Loading) {
                     CircularProgressIndicator(modifier = Modifier.size(24.dp), color = MaterialTheme.colorScheme.onPrimary)
                 } else {
-                    Text("Anmelden")
+                    Text(
+                        text = stringResource(id = R.string.login)
+                    )
                 }
             }
+
 
             if (loginState is LoginState.Error) {
                 Text(
@@ -99,7 +115,7 @@ fun Home(navController: NavController) {
             Spacer(modifier = Modifier.height(48.dp))
 
             Text(
-                text = "Noch nicht registriert?",
+                text = stringResource(id = R.string.not_registered_yet),
                 fontSize = 18.sp
             )
 
@@ -110,7 +126,7 @@ fun Home(navController: NavController) {
                 shape = RectangleShape
             ) {
                 Text(
-                    text ="Zur Registrierung"
+                    text = stringResource(id = R.string.go_to_registration)
                 )
             }
         }
